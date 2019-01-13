65ecdcee9854cb267107dbd7f87c3c9c160d16be
stombre <guillaumedaix@free.fr>
2019-01-13 16:33:04 +0100

    ADD Schema field variables

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const DATABASES = require('./lib/databases');
 const MODEL = require('./lib/model');
 const QUERY = require('./lib/query');
+const SCHEMA_FIELD = require('./lib/schemaField');
 
 module.exports = {
   DATABASES,
   MODEL,
   QUERY,
+  SCHEMA_FIELD,
 };
