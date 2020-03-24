b0d178ccaf2e1bbf3cff11edc6aa9056fec2c840
daix <evirtuel@gmail.com>
2020-03-24 12:54:58 +0100

    MOD change primary key list to schema (and not model)

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const DATABASES = require('./lib/databases');
 const MODEL = require('./lib/model');
 const QUERY = require('./lib/query');
+const SCHEMA = require('./lib/schema');
 const SCHEMA_FIELD = require('./lib/schemaField');
 
 module.exports = {
   DATABASES,
   MODEL,
   QUERY,
+  SCHEMA,
   SCHEMA_FIELD,
 };
