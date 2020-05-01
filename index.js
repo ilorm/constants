9d234f7cf9d3ba9f5539470b6299dc81aa712ced
daix <evirtuel@gmail.com>
2020-05-01 20:07:21 +0200

    MOD rename schemaField to field

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const DATABASES = require('./lib/databases');
 const MODEL = require('./lib/model');
 const QUERY = require('./lib/query');
 const SCHEMA = require('./lib/schema');
-const SCHEMA_FIELD = require('./lib/schemaField');
+const FIELD = require('./lib/field');
 
 module.exports = {
   DATABASES,
   MODEL,
   QUERY,
   SCHEMA,
-  SCHEMA_FIELD,
+  FIELD,
 };
